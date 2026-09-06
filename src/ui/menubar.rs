cf5ad0a89a4babf329934670e8aa250146c436d7
KATOI4423 <katoi4423@gmail.com>
2026-09-06 16:13:22 +0900

    Add Help pulldown to menubar

--- src/ui/menubar.rs
+++ src/ui/menubar.rs
@@ -3,7 +3,9 @@
 //! Define the menu bar for the Doors application.
 
 use gpui::*;
+use gpui::prelude::FluentBuilder;
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 enum MenuBarState {
     AllClosed,
     HelpOpen,
@@ -18,6 +20,55 @@ impl MenuBar {
         Self {
             state: MenuBarState::AllClosed,
         }
+    }
+
+    fn create_help_menu(&mut self, cx: &mut Context<Self>) -> impl IntoElement {
+        div()
+            .relative()
+            .h_full()
+            .flex()
+            .items_start()
+            .justify_start()
+            .child(
+                div()
+                    .id("help")
+                    .px_2()
+                    .child("Help")
+                    .on_click(cx.listener(|this, _, _, _| {
+                        if this.state == MenuBarState::HelpOpen {
+                            this.state = MenuBarState::AllClosed;
+                        } else {
+                            this.state = MenuBarState::HelpOpen;
+                        }
+                    })),
+            ).when(self.state == MenuBarState::HelpOpen, |this| {
+                this.child(
+                    div()
+                        .absolute()
+                        .top(px(32.0))
+                        .left_0()
+                        .w(px(160.0))
+                        .bg(rgb(0x303030))
+                        .border_1()
+                        .border_color(rgb(0x505050))
+                        .flex()
+                        .flex_col()
+                        .child(
+                            div()
+                                .id("read-me")
+                                .px_3()
+                                .py_2()
+                                .child("Read Me"),
+                        )
+                        .child(
+                            div()
+                                .id("version")
+                                .px_3()
+                                .py_2()
+                                .child("Version"),
+                        ),
+                )
+            })
     }
 }
 
@@ -35,6 +86,6 @@ impl Render for MenuBar {
             .pl_2() // pading-left
             .gap_4()
             .child("Menu Bar 1")
-            .child("Menu Bar 2")
+            .child(self.create_help_menu(cx))
     }
 }
