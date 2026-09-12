6df500eaf5d93232dc97945ed4f843f1410d7d63
KATOI4423 <katoi4423@gmail.com>
2026-09-12 17:58:34 +0900

    Add window resize handler

--- src/ui/mainwindow.rs
+++ src/ui/mainwindow.rs
@@ -30,6 +30,7 @@ impl Render for MainWindow {
             }))
             .child(self.titlebar.clone())
             .child(MainWindow::render_main_content())
+            .child(Self::render_resize_handles())
     }
 }
 
@@ -74,6 +75,86 @@ impl MainWindow {
         div()
             .flex()
             .child("Main Content")
+    }
+
+    /// # Render Resize Handlers
+    ///
+    /// 4辺と4角にウィンドウサイズ変更用ハンドラを設定する
+    fn render_resize_handles() -> impl IntoElement {
+        div()
+            .absolute()
+            .inset_0()
+            .children([
+                Self::render_resize_handle(ResizeEdge::Top),
+                Self::render_resize_handle(ResizeEdge::Bottom),
+                Self::render_resize_handle(ResizeEdge::Left),
+                Self::render_resize_handle(ResizeEdge::Right),
+                Self::render_resize_handle(ResizeEdge::TopLeft),
+                Self::render_resize_handle(ResizeEdge::TopRight),
+                Self::render_resize_handle(ResizeEdge::BottomLeft),
+                Self::render_resize_handle(ResizeEdge::BottomRight),
+            ])
+    }
+
+    fn render_resize_handle(edge: ResizeEdge) -> impl IntoElement {
+        // 共通部分
+        const HANDLER_PIXEL_SIZE: f32 = 12.0;
+        let cursor_style = match edge {
+            ResizeEdge::Top | ResizeEdge::Bottom => CursorStyle::ResizeUpDown,
+            ResizeEdge::Left | ResizeEdge::Right => CursorStyle::ResizeLeftRight,
+            ResizeEdge::TopLeft | ResizeEdge::BottomRight => CursorStyle::ResizeUpLeftDownRight,
+            ResizeEdge::TopRight | ResizeEdge::BottomLeft => CursorStyle::ResizeUpRightDownLeft,
+        };
+        let div = div()
+            .absolute()
+            .cursor(cursor_style)
+            .on_mouse_down(MouseButton::Left, move |_, window, cx| {
+                cx.stop_propagation();
+                window.start_window_resize(edge);
+            });
+
+        match edge {
+            ResizeEdge::Top => div
+                .top_0()
+                .left(px(HANDLER_PIXEL_SIZE))
+                .right(px(HANDLER_PIXEL_SIZE))
+                .h(px(HANDLER_PIXEL_SIZE)),
+            ResizeEdge::Bottom => div
+                .bottom_0()
+                .left(px(HANDLER_PIXEL_SIZE))
+                .right(px(HANDLER_PIXEL_SIZE))
+                .h(px(HANDLER_PIXEL_SIZE)),
+            ResizeEdge::Left => div
+                .left_0()
+                .top(px(HANDLER_PIXEL_SIZE))
+                .bottom(px(HANDLER_PIXEL_SIZE))
+                .w(px(HANDLER_PIXEL_SIZE)),
+            ResizeEdge::Right => div
+                .right_0()
+                .top(px(HANDLER_PIXEL_SIZE))
+                .bottom(px(HANDLER_PIXEL_SIZE))
+                .w(px(HANDLER_PIXEL_SIZE)),
+            ResizeEdge::TopLeft => div
+                .top_0()
+                .left_0()
+                .h(px(HANDLER_PIXEL_SIZE))
+                .w(px(HANDLER_PIXEL_SIZE)),
+            ResizeEdge::TopRight => div
+                .top_0()
+                .right_0()
+                .h(px(HANDLER_PIXEL_SIZE))
+                .w(px(HANDLER_PIXEL_SIZE)),
+            ResizeEdge::BottomLeft => div
+                .bottom_0()
+                .left_0()
+                .h(px(HANDLER_PIXEL_SIZE))
+                .w(px(HANDLER_PIXEL_SIZE)),
+            ResizeEdge::BottomRight => div
+                .bottom_0()
+                .right_0()
+                .h(px(HANDLER_PIXEL_SIZE))
+                .w(px(HANDLER_PIXEL_SIZE)),
+        }
     }
 }
 
