9411c7e6d57a4f5b83208f9e3dfec774152359a5
KATOI4423 <katoi4423@gmail.com>
2026-09-20 16:08:20 +0900

    Add features to synchronize the inputed path and back/forward button

--- src/ui/filelist.rs
+++ src/ui/filelist.rs
@@ -5,7 +5,10 @@
 use std::collections::VecDeque;
 use std::path::PathBuf;
 
-use gpui_component::input::Input;
+use gpui_component::input::{
+    Input,
+    InputEvent,
+};
 use gpui_kit::base::input::InputState;
 use gpui_kit::*;
 
@@ -35,12 +38,35 @@ impl FileList {
                 .default_value(path.to_string_lossy())
         });
 
-        Self {
+        let filelist = Self {
             current: path,
             back: VecDeque::default(), // TODO: 履歴復元機能を追加
             forward: VecDeque::default(), // TODO: 履歴復元機能を追加
             path_input,
-        }
+        };
+
+        filelist.create_subscribe(window, cx);
+
+        filelist
+    }
+
+    fn create_subscribe(&self, window: &mut Window, cx: &mut Context<Self>) {
+        // path_input のイベント処理
+        cx.subscribe_in(&self.path_input, window, |this, _state, event, window, cx| {
+            match event {
+                InputEvent::PressEnter { secondary: _, shift: _ } => {
+                    let value = this.path_input.read(cx).value();
+                    let path = PathBuf::from(value.to_string());
+
+                    // TODO: パス移動処理 (移動先があるかどうかをチェック？)
+                    dbg!(&path);
+                    Self::update_stack(&mut this.back, this.current.clone());
+                    this.set_current_path(window, cx, path);
+                    this.forward.clear();
+                },
+                _ => {},
+            }
+        }).detach();
     }
 
     fn render_toolbar(&self, window: &mut Window, cx: &mut Context<Self>) -> impl IntoElement {
@@ -74,6 +100,13 @@ impl FileList {
         stack.push_back(element);
     }
 
+    fn set_current_path(&mut self, window: &mut Window, cx: &mut Context<Self>, current: impl Into<PathBuf>) {
+        self.current = current.into();
+        self.path_input.update(cx, |input, cx| {
+            input.set_value(self.current.to_string_lossy(), window, cx);
+        })
+    }
+
     fn render_button(
         id: impl Into<ElementId>,
         icon_path: impl Into<SharedString>,
@@ -95,7 +128,7 @@ impl FileList {
     }
 
     fn render_back_button(_window: &Window, cx: &mut Context<Self>) -> impl IntoElement {
-        Self::render_button("back", "icons/chevron-left.svg", cx.listener(|this, _, _, _| {
+        Self::render_button("back", "icons/chevron-left.svg", cx.listener(|this, _event, window, cx| {
             let Some(back) = this.back.pop_back() else {
                 eprintln!("back is empty");
                 return;
@@ -103,12 +136,12 @@ impl FileList {
 
             dbg!(&back);
             Self::update_stack(&mut this.forward, this.current.clone());
-            this.current = back;
+            this.set_current_path(window, cx, back);
         }))
     }
 
     fn render_front_button(_window: &Window, cx: &mut Context<Self>) -> impl IntoElement {
-        Self::render_button("front", "icons/chevron-right.svg", cx.listener(|this, _, _, _| {
+        Self::render_button("front", "icons/chevron-right.svg", cx.listener(|this, _event, window, cx| {
             let Some(forward) = this.forward.pop_back() else {
                 eprintln!("forward is empty");
                 return;
@@ -116,7 +149,7 @@ impl FileList {
 
             dbg!(&forward);
             Self::update_stack(&mut this.back, this.current.clone());
-            this.current = forward;
+            this.set_current_path(window, cx, forward);
         }))
     }
 
@@ -124,7 +157,7 @@ impl FileList {
     ///
     /// Note: forward の履歴は削除する
     fn render_parent_button(_window: &Window, cx: &mut Context<Self>) -> impl IntoElement {
-        Self::render_button("parent", "icons/allow-up.svg", cx.listener(|this, _, _, _| {
+        Self::render_button("parent", "icons/allow-up.svg", cx.listener(|this, _event, window, cx| {
             let Some(parent) = this.current.parent() else {
                 eprintln!("there are no parent directory");
                 return;
@@ -133,7 +166,7 @@ impl FileList {
             dbg!(&parent);
             this.forward.clear();
             Self::update_stack(&mut this.back, this.current.clone());
-            this.current = parent.to_owned();
+            this.set_current_path(window, cx, parent.to_owned());
         }))
     }
 
